e9098a0314c8d76fe228000e63bbb0f6d36ce5c0
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-09-28 19:01:08 +0530

    Create p2.c

---
+++ Task1/p2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("enter no");
+  scanf("%d",&a);
+  for(int j=1;j<=a;j++)
+    {
+      for(int k=1;k<=a;k++)
+        {
+          printf("1");
+        }
+      printf("\n");
+    }
+}
