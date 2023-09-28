c83f4505008c396ea3a5af7f84363b064a5a5709
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-09-28 20:33:01 +0530

    Update p5.c

--- Task1/p5.c
+++ Task1/p5.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 int main()
 {
-  int a;
+  int a,z=0;
   printf("Enter no");
   scanf("%d",&a);
-  for(int l=a;l>=1;l--)
+  for(int u=a;u>=1;u--)
     {
-      for(int m=1;m<=l;m++)
+       for(int v=1;v<=u;v++)
         {
-         printf("%d ",m);
+         printf("%d ",v);
+        } 
+        printf("\n");
+        z=z+2;
+        for(int w=1;w<=z;w++)
+        {
+           printf(" "); 
         }
-      printf("\n");
+      
     }
 }
