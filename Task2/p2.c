3ac3fb365a6a7e5b84b0f479141902e82b43e3ed
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-09-28 21:54:06 +0530

    Create p2.c

---
+++ Task2/p2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int a[5];
+  printf("Enter the array");
+  for(int i=0;i<=4;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+  for(int j=0;j<=4;j++)
+    {
+      a[j] = 15 + a[j];
+    }
+   printf("Resultant array");
+  for(int k=0;k<=4;k++)
+    {
+      printf("%d ",a[k]);
+    }
+}
