c800cce05b2ef7f538127347adeb0718ea485847
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-09-28 22:09:44 +0530

    Create p3.c

---
+++ Task2/p3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter the size of array");
+  scanf("%d",&a);
+  int b[a],c[a],d[a];
+  printf("array1 element ");
+  for(int i=0;i<a;i++)
+    {
+      scanf("%d",&b[i]);
+    }
+   printf("array2 element ");
+  for(int j=0;j<a;j++)
+    {
+      scanf("%d",&c[j]);
+    }
+  for(int k=0;k<a;k++)
+      {
+        d[k]=b[k]+c[k];
+      }
+  printf("Sum is ");
+  for(int m=0;m<a;m++)
+    {
+      printf("%d\n",d[m]);
+    }
+}
