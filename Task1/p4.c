faeda5173dcb7fabca9b17c6cfc4483991ce8aa0
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-09-28 19:45:36 +0530

    Create p4.c

---
+++ Task1/p4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter no");
+  scanf("%d",&a);
+  for(int l=1;l<=a;l++)
+    {
+      for(int m=1;m<=l;m++)
+        {
+         printf("%d ",m);
+        }
+      printf("\n");
+    }
+}
