35f109246d7aa1ec63789e479a9306531afff911
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-09-28 19:55:34 +0530

    Create p5.c

---
+++ Task1/p5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter no");
+  scanf("%d",&a);
+  for(int l=a;l>=1;l--)
+    {
+      for(int m=1;m<=l;m++)
+        {
+         printf("%d ",m);
+        }
+      printf("\n");
+    }
+}
